919284b5c74a09a7941e8e11e003729e8d4bcb38
SYANN.CO <meishan953.ms@gmail.com>
2026-05-12 22:48:40 +0800

    update energy quiz page

--- app/energy-quiz/page.tsx
+++ app/energy-quiz/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import Navbar from '@/components/Navbar'
+import Navbar from '../../components/Navbar'
 import './energy-quiz.css'
 
 export default function EnergyQuizPage() {
