2ff2bd18715256a52b93b96ed5cc9f22f83dbed0
meishan.chan <meishan953.ms@gmail.com>
2026-05-19 03:21:16 +0800

    update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,24 +32,19 @@ const LINK_STYLE: React.CSSProperties = {
 }
 
 const SHOP_LINKS = [
-  { href: '/collection',    label: 'All Bracelets' },
-  { href: '/how-it-works',  label: 'How It Works' },
-  { href: '/best-sellers',  label: 'Best Sellers' },
-  { href: '/gift-cards',    label: 'Gift Cards' },
+  { href: '/collection',   label: 'All Bracelets' },
+  { href: '/how-it-works', label: 'How It Works' },
 ]
 
 const ABOUT_LINKS = [
-  { href: '/about',         label: 'Our Story' },
-  { href: '/five-elements', label: 'The Five Elements' },
-  { href: '/crystals',      label: 'Crystal Guide' },
-  { href: '/faq',           label: 'FAQ' },
+  { href: '/about', label: 'Our Story' },
+  { href: '/faq',   label: 'FAQ' },
 ]
 
 const CARE_LINKS = [
-  { href: '/order-tracking', label: 'Track Your Order' },
-  { href: '/shipping',       label: 'Shipping & Delivery' },
-  { href: '/returns',        label: 'Returns & Exchanges' },
-  { href: '/contact',        label: 'Contact Us' },
+  { href: '/shipping', label: 'Shipping & Delivery' },
+  { href: '/returns',  label: 'Returns & Exchanges' },
+  { href: '/contact',  label: 'Contact Us' },
 ]
 
 const SOCIAL_LINKS = [
@@ -65,20 +60,11 @@ const SOCIAL_LINKS = [
     ),
   },
   {
-    href: 'https://facebook.com/syannco',
-    label: 'Facebook',
+    href: 'https://tiktok.com/@syann.co',
+    label: 'TikTok',
     icon: (
       <svg width="15" height="15" viewBox="0 0 24 24" fill="currentColor">
-        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/>
-      </svg>
-    ),
-  },
-  {
-    href: 'https://pinterest.com/syann_co',
-    label: 'Pinterest',
-    icon: (
-      <svg width="15" height="15" viewBox="0 0 24 24" fill="currentColor">
-        <path d="M12 0C5.373 0 0 5.373 0 12c0 5.084 3.163 9.426 7.627 11.174-.105-.949-.2-2.405.042-3.441.218-.937 1.407-5.965 1.407-5.965s-.359-.719-.359-1.782c0-1.668.967-2.914 2.171-2.914 1.023 0 1.518.769 1.518 1.69 0 1.029-.655 2.568-.994 3.995-.283 1.194.599 2.169 1.777 2.169 2.133 0 3.772-2.249 3.772-5.495 0-2.873-2.064-4.882-5.012-4.882-3.414 0-5.418 2.561-5.418 5.207 0 1.031.397 2.138.893 2.738a.36.36 0 0 1 .083.345l-.333 1.36c-.053.22-.174.267-.402.161-1.499-.698-2.436-2.889-2.436-4.649 0-3.785 2.75-7.262 7.929-7.262 4.163 0 7.398 2.967 7.398 6.931 0 4.136-2.607 7.464-6.227 7.464-1.216 0-2.359-.632-2.75-1.378l-.748 2.853c-.271 1.043-1.002 2.35-1.492 3.146C9.57 23.812 10.763 24 12 24c6.627 0 12-5.373 12-12S18.627 0 12 0z"/>
+        <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-2.88 2.5 2.89 2.89 0 0 1-2.89-2.89 2.89 2.89 0 0 1 2.89-2.89c.28 0 .54.04.79.1V9.01a6.32 6.32 0 0 0-.79-.05 6.34 6.34 0 0 0-6.34 6.34 6.34 6.34 0 0 0 6.34 6.34 6.34 6.34 0 0 0 6.33-6.34V8.95a8.16 8.16 0 0 0 4.78 1.52V7.04a4.85 4.85 0 0 1-1.01-.35z"/>
       </svg>
     ),
   },
@@ -125,7 +111,7 @@ export default function Footer() {
     <footer
       style={{
         width: '100%',
-        background: '#2C201A',
+        background: '#3D2B1F',
         ...BODY,
       }}
     >
